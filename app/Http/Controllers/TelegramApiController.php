d4917adb48b2a443032663a7ebeedad566a5d5c5
numetaliev <numetaliev@nurtelecom.kg>
2022-05-10 21:21:52 +0600

    Telegram SDK: Предоставление информации о номере телефона

--- app/Http/Controllers/TelegramApiController.php
+++ app/Http/Controllers/TelegramApiController.php
@@ -5,17 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Telegram\Bot\Api;
 use Telegram\Bot\Exceptions\TelegramSDKException;
-//use Telegram\Bot\Objects\User;
+use Telegram\Bot\Keyboard\Keyboard;
 
 class TelegramApiController extends Controller
 {
     private Api $Telegram;
-//    private User $Response;
 
     public function __construct()
     {
         $this->Telegram = new Api();
-//        $this->Response = $this->Telegram->getMe();
     }
 
     /**
@@ -23,16 +21,33 @@ class TelegramApiController extends Controller
      */
     public function handle(Request $request)
     {
-        $res = $this->Telegram->getMe();
-
+        $data = $request->all();
         $this->Telegram->sendMessage([
             'chat_id' => '1327706165',
-            'text' => json_encode($res, JSON_PRETTY_PRINT)
+            'text' => json_encode($data, JSON_PRETTY_PRINT)
         ]);
 
-        $this->Telegram->sendMessage([
-            'chat_id' => '1327706165',
-            'text' => json_encode($request->all(), JSON_PRETTY_PRINT)
-        ]);
+        $message = $data['message'];
+        if (empty($message)) return;
+        if ($message['text'] === 'start') {
+            $keyboard = Keyboard::make([
+                'keyboard' => [
+                    [
+                        [
+                            'text' => 'Предоставить номер телефона',
+                            'request_contact' => true
+                        ]
+                    ]
+                ],
+                'resize_keyboard' => true,
+                'one_time_keyboard' => true
+            ]);
+
+            $this->Telegram->sendMessage([
+                'chat_id' => '1327706165',
+                'text' => 'Пожалуйста, поделитесь вашим номером телефона',
+                'reply_markup' => $keyboard
+            ]);
+        }
     }
 }
