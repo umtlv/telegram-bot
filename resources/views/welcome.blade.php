7d130f934e19c896244b3d14cc5294536419c15d
numetaliev <numetaliev@nurtelecom.kg>
2022-05-10 19:30:11 +0600

    initial release

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Telegram Bot API</title>
+</head>
+<body>Telegram Bot API</body>
+</html>
