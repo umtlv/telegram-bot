8ad15076223b2cfbc09a1e4afc28f3df94ab7f02
numetaliev <numetaliev@nurtelecom.kg>
2022-05-12 09:58:09 +0600

    Telegram API: Информация о профиле

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/log', function () {
+    return File::get('../storage/logs/laravel.log');
+});
+Route::get('/log-delete', function () {
+    File::delete('../storage/logs/laravel.log');
 });
